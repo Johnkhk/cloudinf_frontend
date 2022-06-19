52d40b13d81a76ac5b4a8cfe6703a02a1c6efd28
John Ho <john@Johns-MBP.attlocal.net>
2022-06-18 18:03:45 -0700

    commit in submodule

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+// axios.defaults.withCredentials = true
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
