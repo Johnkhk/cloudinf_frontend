153e0141ef34f4a59690919840b5100460765a1d
John Ho <john@Johns-MacBook-Pro.local>
2022-09-06 18:00:51 +0800

    movie reccomender failed build

---
+++ pages/moviereccomender.js
@@ -0,0 +1,15 @@
+const MovieReccomender = () => {
+    return(
+        <>
+            <div className="flex flex-col h-screen "> 
+                <div className="flex flex-col h-screen">
+                    <div className="flex flex-row justify-center">
+                        SORRY WORK IN PROGRESS
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+export default MovieReccomender
